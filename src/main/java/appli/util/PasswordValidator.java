e777fdd0355d1a0abc887bfa471e693adbc23b0c
TOUIDJINi Mehdi <mehditoudjay@gmail.com>
2026-03-02 14:07:16 +0100

    Ajout classe validation mot de passe user

---
+++ src/main/java/appli/util/PasswordValidator.java
@@ -0,0 +1,25 @@
+package appli.util;
+
+public class PasswordValidator {
+
+
+    // Méthode principale de validation
+    public static boolean isValid(String password) {
+        if (password == null) {
+            return false;
+        }
+
+        // Minimum 8 caractères
+        if (password.length() < 8) {
+            return false;
+        }
+
+        // Vérifications avec regex
+        boolean hasUppercase = password.matches(".*[A-Z].*");
+        boolean hasLowercase = password.matches(".*[a-z].*");
+        boolean hasDigit = password.matches(".*\\d.*");
+        boolean hasSpecialChar = password.matches(".*[@#$%^&+=!].*");
+
+        return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+    }
+}
